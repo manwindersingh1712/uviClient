392b1518fb8778cec1e5ca1cfcb64c772ecb8d8c
Manwinder Singh <manwindersingh1712@gmail.com>
2021-09-26 03:34:58 +0530

    updated image

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,10 +2,14 @@ import React from "react";
 import MyCarousel from "../components/carousle";
 import Form from "../components/form";
 import rightCorner from "../assets/images/homepage/r-corner.png";
+import leftCorner from "../assets/images/homepage/l-corner.png";
 
 const HomePage = () => {
   return (
-    <div className="md:container md:mx-auto flex flex-col justify-around md:flex-row mt-20">
+    <div className="container mx-auto flex flex-col justify-around md:flex-row mt-20">
+      <div className="hidden md:block absolute bottom-16 left-0">
+        <img src={leftCorner} alt="right corner" />
+      </div>
       <div className="w-4/4">
         <Form />
       </div>
