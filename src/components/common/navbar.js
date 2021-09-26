2590183c747845e08758ba7b4b4bde3b4e205f88
Manwinder Singh <manwindersingh1712@gmail.com>
2021-09-26 18:52:04 +0530

    navbar responsive

--- src/components/common/navbar.js
+++ src/components/common/navbar.js
@@ -5,8 +5,8 @@ import { withRouter } from "react-router-dom";
 const Navbar = (props) => {
   const { history } = props;
   return (
-    <div className="border-b-2 border-solid border-gray-300 h-28">
-      <div className="container mx-auto flex justify-between items-center">
+    <div className="border-b-2 border-solid border-gray-300 h-28 px-10 md:p-0">
+      <div className="container mx-auto flex justify-between items-center h-full">
         <div
           className="w-14 md:w-24 py-4 cursor-pointer"
           onClick={() => {
@@ -17,7 +17,7 @@ const Navbar = (props) => {
         </div>
         <div>
           <button
-            className="w-max bg-grey-10 text-white font-bold px-8 py-4 montserrat rounded-full hover:bg-red-400 mr-8"
+            className="w-max bg-grey-10 text-white font-bold text-sm md:text-base px-4 py-2 md:px-8 md:py-4 montserrat rounded-full hover:bg-red-400 mr-1 md:mr-8"
             onClick={() => {
               history.push("/day_view");
             }}
@@ -25,7 +25,7 @@ const Navbar = (props) => {
             Day
           </button>
           <button
-            className="w-max bg-grey-10 text-white font-bold px-8 py-4 montserrat rounded-full hover:bg-red-400"
+            className="w-max bg-grey-10 text-white font-bold text-sm md:text-base px-4 py-2 md:px-8 md:py-4 montserrat rounded-full hover:bg-red-400 mr-1 md:mr-8"
             onClick={() => {
               history.push("/month_view");
             }}
